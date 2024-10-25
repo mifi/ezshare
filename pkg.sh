6a95d366b133af0766143977014b344fd1913d3b
Mikael Finstad <finstaden@gmail.com>
2024-10-25 20:32:49 +0200

    fix script

--- pkg.sh
+++ pkg.sh
@@ -4,9 +4,9 @@ cp node_modules/clipboardy/fallbacks/windows/clipboard_x86_64.exe pkg/win/ezshar
 cp node_modules/clipboardy/fallbacks/linux/xsel pkg/linux/ezshare/clipboard
 #rm -r node_modules/clipboardy/fallbacks
 yarn pkg -t node18-win-x64,node18-macos-arm64,node18-linux-x64 .
-mv ezshare-win.exe pkg/win/ezshare/ezshare.exe
-mv ezshare-linux pkg/linux/ezshare/ezshare
-mv ezshare-macos pkg/macos/ezshare
-(cd win && zip -r ../ezshare-win.zip ezshare)
-(cd macos && zip ../ezshare-macos.zip ezshare)
-(cd linux && zip -r ../ezshare-linux.zip ezshare)
+mv ezshare-win-x64.exe pkg/win/ezshare/ezshare.exe
+mv ezshare-linux-x64 pkg/linux/ezshare/ezshare
+mv ezshare-macos-arm64 pkg/macos/ezshare
+(cd pkg/win && zip -r ../../ezshare-win.zip ezshare)
+(cd pkg/macos && zip ../../ezshare-macos.zip ezshare)
+(cd pkg/linux && zip -r ../../ezshare-linux.zip ezshare)
